9b434e251cdb3294fe51a0a9d81af854ad9ef90e
Emma Westerhoff <emma.westerhoff@students.olin.edu>
2017-04-15 16:25:26 -0400

    third iteration - function format

--- third iteration/fromMidi3.py
+++ third iteration/fromMidi3.py
@@ -1,91 +1,70 @@
 import mido
 import csv
 
-mid = mido.MidiFile('DNAmusic3.mid')
+def readInMidi(myFile):
+    mid = mido.MidiFile(myFile)
 
-notes = []
-noteLengths = []
-for msg in mid.play():
-    notes.append(msg.note)
-    noteLengths.append(msg.time)
+    notes = []
+    noteLengths = []
+    for msg in mid.play():
+        notes.append(msg.note)
+        noteLengths.append(msg.time)
+    return notes, noteLengths
 
-lengthSong = int(len(notes))
+def createFirstTwoAcids(notes):
+    lengthSong = int(len(notes))
 
-for i in range(0, lengthSong//2):
-    notes[i] = notes[i*2]
+    for i in range(0, lengthSong//2):
+        notes[i] = notes[i*2]
 
-durations = []
-blanks = []
+    sequence = []
+    sequenceU = []
+    startingPitch = 60 #sets the key to C major
+    notesInKey = [0,2,4,5,7,9,11,12,14,16,17,19,21,23,24,26]
+    acidsList = ['AA', 'AT', 'AC', 'AG',
+                 'TA', 'TT', 'TC', 'TG',
+                 'CA', 'CT', 'CC', 'CG',
+                 'GA', 'GT', 'GC', 'GG']
+    possibleNumNotes = int(len(notesInKey))
+    for pitch in notes:
+        for i in range(0, possibleNumNotes):
+            if pitch == startingPitch+notesInKey[i]:
+                acid = acidsList[i]
+        sequence.append(acid)
+    return sequence
 
-# for i in range(lengthOfNoteLengths):
-#     if noteLengths[i] != 0
-#         durations.append(noteLengths[i])
-# print(durations)
+def createThirdAcid(noteLengths):
+    sequenceThird = []
+    for l in noteLengths:
+        if l == .25:
+            thirdAcid = 'C'
+            sequenceThird.append(thirdAcid)
+        if l == .5:
+            thirdAcid = 'A'
+            sequenceThird.append(thirdAcid)
+        if l == .75:
+            thirdAcid = 'G'
+            sequenceThird.append(thirdAcid)
+        if l == 1:
+            thirdAcid = 'T'
+            sequenceThird.append(thirdAcid)
+    return sequenceThird
 
-sequence = []
-sequenceU = []
-startingPitch = 60 #sets the key to C major
-for pitch in notes:
-    if pitch == startingPitch + 0:
-        acid = 'AA'
-    if pitch == startingPitch + 2:
-        acid = 'AT'
-    if pitch == startingPitch + 4:
-        acid = 'AC'
-    if pitch == startingPitch + 5:
-        acid = 'AG'
+def writeToText(data):
+    file = open("DNAfromMidi3.txt", "w")
+    for i in data:
+        file.write(i+"")
+    file.close()
 
-    if pitch == startingPitch + 7:
-        acid = 'TA'
-    if pitch == startingPitch + 9:
-        acid = 'TT'
-    if pitch == startingPitch + 11:
-        acid = 'TC'
-    if pitch == startingPitch + 12:
-        acid = 'TG'
 
-    if pitch == startingPitch + 14:
-        acid = 'CA'
-    if pitch == startingPitch + 16:
-        acid = 'CT'
-    if pitch == startingPitch + 17:
-        acid = 'CC'
-    if pitch == startingPitch + 19:
-        acid = 'CG'
-
-    if pitch == startingPitch + 21:
-        acid = 'GA'
-    if pitch == startingPitch + 23:
-        acid = 'GT'
-    if pitch == startingPitch + 24:
-        acid = 'GC'
-    if pitch == startingPitch + 26:
-        acid = 'GG'
-    sequence.append(acid)
-
-for l in noteLengths:
-    if l == .25:
-        thirdAcid = 'C'
-        sequenceU.append(thirdAcid)
-    if l == .5:
-        thirdAcid = 'A'
-        sequenceU.append(thirdAcid)
-    if l == .75:
-        thirdAcid = 'G'
-        sequenceU.append(thirdAcid)
-    if l == 1:
-        thirdAcid = 'T'
-        sequenceU.append(thirdAcid)
-
+myNotes, myNoteLengths = readInMidi('DNAmusic3.mid')
+firstTwo = createFirstTwoAcids(myNotes)
+third = createThirdAcid(myNoteLengths)
 finalSequence = []
-lengthSequence = int(len(sequenceU))
+lengthSequence = int(len(third))
 
 for a in range(0, lengthSequence):
-    nuc = sequence[a] + sequenceU[a]
+    nuc = firstTwo[a] + third[a]
     finalSequence.append(nuc)
-print(finalSequence)
 
-file = open("DNAfromMidi3.txt", "w")
-for i in finalSequence:
-    file.write(i+"")
-file.close()
+writeToText(finalSequence)
--- third iteration/toMidi3.py
+++ third iteration/toMidi3.py
@@ -2,74 +2,53 @@
 import csv
 from midiutil import MIDIFile
 
-#first iteration
-#results = ['A', 'G', 'T', 'C', 'A', 'G', 'G', 'T', 'C', 'A', 'A', 'C']
+def formatDNAFile(rawFile):
+    results = []
+    with open(rawFile) as f:
+      while True:
+        c = f.read(3)
+        if not c:
+          break
+        results.append(c)
+    return results
 
-results = []
-with open("sampleDNA6.txt") as f:
-  while True:
-    c = f.read(3)
-    if not c:
-      break
-    results.append(c)
 
-#degrees  = [60, 62, 64, 65, 67, 69, 71, 72]  # MIDI note number
-degrees = []
-speeds = []
-for a in results:
-    i = a[0:2] #determines pitch
-    c = a[2] #determines...something?
+
+def textToPitches(dnaText):
+    degrees = []
+    speeds = []
     startingPitch = 60 #determines key 60 = C MAJOR
+    notesInKey = [0,2,4,5,7,9,11,12,14,16,17,19,21,23,24,26]
+    acidsList = ['AA', 'AT', 'AC', 'AG',
+                 'TA', 'TT', 'TC', 'TG',
+                 'CA', 'CT', 'CC', 'CG',
+                 'GA', 'GT', 'GC', 'GG']
+    possibleNumAcidCombos = int(len(acidsList))
+    for a in dnaText:
+        i = a[0:2] #determines pitch
+        c = a[2] #determines...something?
 
-    if i == 'AA':
-        pitch = startingPitch + 0
-    if i == 'AT':
-        pitch = startingPitch + 2
-    if i == 'AC':
-        pitch = startingPitch + 4
-    if i == 'AG':
-        pitch = startingPitch + 5
+        for x in range(0, possibleNumAcidCombos):
+            if i == acidsList[x]:
+                pitch = startingPitch + notesInKey[x]
 
-    if i == 'TA':
-        pitch = startingPitch + 7
-    if i == 'TT':
-        pitch = startingPitch + 9
-    if i == 'TC':
-        pitch = startingPitch + 11
-    if i == 'TG':
-        pitch = startingPitch + 12
+        if c == 'A':
+            duration = 1
+        if c == 'T':
+            duration = 2
+        if c == 'C':
+            duration = .5
+        if c == 'G':
+            duration = 1.5
 
-    if i == 'CA':
-        pitch = startingPitch + 14
-    if i == 'CT':
-        pitch = startingPitch + 16
-    if i == 'CC':
-        pitch = startingPitch + 17
-    if i == 'CG':
-        pitch = startingPitch + 19
+        degrees.append(pitch)
+        speeds.append(duration)
+    return degrees, speeds
 
-    if i == 'GA':
-        pitch = startingPitch + 21
-    if i == 'GT':
-        pitch = startingPitch + 23
-    if i == 'GC':
-        pitch = startingPitch + 24
-    if i == 'GG':
-        pitch = startingPitch + 26
 
-    if c == 'A':
-        duration = 1
-    if c == 'T':
-        duration = 2
-    if c == 'C':
-        duration = .5
-    if c == 'G':
-        duration = 1.5
+formattedForInsert = formatDNAFile("sampleDNA6.txt")
+myDegrees, mySpeeds = textToPitches(formattedForInsert)
 
-    degrees.append(pitch)
-    speeds.append(duration)
-
-print(degrees)
 track    = 0
 channel  = 0
 time     = 0    # In beats
@@ -81,9 +60,9 @@ MyMIDI = MIDIFile(1, adjust_origin = True)  # One track
 MyMIDI.addTempo(track, time, tempo)
 currentTime = time
 
-for i, pitch in enumerate(degrees):
-    MyMIDI.addNote(track, channel, pitch, currentTime, speeds[i], volume)
-    currentTime = currentTime+speeds[i]
+for i, pitch in enumerate(myDegrees):
+    MyMIDI.addNote(track, channel, pitch, currentTime, mySpeeds[i], volume)
+    currentTime = currentTime+mySpeeds[i]
 
 with open("DNAmusic3.mid", "wb") as output_file:
     MyMIDI.writeFile(output_file)
